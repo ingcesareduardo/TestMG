cc6690983872c189cb23f687bb7aacb352892347
Cesar Rodriguez <ceroqui10@hotmail.com>
2019-09-18 00:32:22 -0500

    Margin bottom de los controles y corrección de versión del core a 2.1

--- TestMG.Web/Startup.cs
+++ TestMG.Web/Startup.cs
@@ -32,7 +32,7 @@ namespace TestMG.Web
             services.AddSingleton<IServiceLocator, ServiceLocator>();
             services.AddSingleton<IWebApiCalls, _webApiCalls>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
